c7313fad65bffa8ccd600e1263e7de7f7f6359bf
Rowaina25 <167467193+Rowaina25@users.noreply.github.com>
2025-03-11 20:40:22 +0200

    Update rag.py

--- rag.py
+++ rag.py
@@ -81,7 +81,6 @@ AVAILABLE_MODELS_DICT = {
     "openai-4o": {"price": "Custom", "type": "openai"},
     "meta-llama/Llama-3.3-70B-Instruct-Turbo": {"price": "$0.88", "type": "together"},
     "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo": {"price": "$3.50", "type": "together"},
-    "databricks/dbrx-instruct": {"price": "$1.20", "type": "together"},
     "microsoft/WizardLM-2-8x22B": {"price": "$1.20", "type": "together"},
     "mistralai/Mixtral-8x22B-Instruct-v0.1": {"price": "$1.20", "type": "together"},
     "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO": {"price": "$0.60", "type": "together"},
@@ -537,7 +536,7 @@ text_color = "#E0E0E0" if is_dark_mode else "#000000"
 user_background = "#333" if is_dark_mode else "#e3f2fd"
 user_text_color = "#FFF" if is_dark_mode else "#000"
 
-st.title("📄 AI Document Q&A and Web Scraper")
+st.title("📄 IntelLaw")
 
 # Sidebar with Tabs
 with st.sidebar:
